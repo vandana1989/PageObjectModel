20a10116005b432a3ffe78b8b16a2b83409040b7
vbarnala <vsunkari@gmail.com>
2020-03-23 17:47:23 -0700

    added comment for abc.java

--- src/test/java/com/apex/kp/signon/constant/LoginConstant.java
+++ src/test/java/com/apex/kp/signon/constant/LoginConstant.java
@@ -6,5 +6,6 @@ public interface LoginConstant {
 	public final static String PASSWORD_LOCATOR ="hsid-password";
 	//public final static String CHROME_DRIVER_PATH="C:\\chromedriver.exe";
 	
+	//test
 	
 }
